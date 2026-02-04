4541527e9466d5f19cf57c15c9eb2201a2524576
mjornada <mauriciojornada@yahoo.com.br>
2026-02-03 21:51:11 -0400

    Tratamento para Satélites Geoestacionários

--- whensat-api/src/main/python/domain/usecase/predictor/satellite_prediction_usecase.py
+++ whensat-api/src/main/python/domain/usecase/predictor/satellite_prediction_usecase.py
@@ -1,27 +1,27 @@
+from typing import Dict
+
 from src.main.python.domain.interfaces.dataprovider.satellite_dataprovider import SatelliteDataProvider
 from src.main.python.domain.services.predictor_service import PredictorService
-from typing import List, Dict
+
 
 class SatellitePredictionUseCase:
-    def __init__(self, 
+    def __init__(self,
                  satellite_dataprovider: SatelliteDataProvider,
                  predictor_service: PredictorService):
         self.satellite_dataprovider = satellite_dataprovider
         self.predictor_service = predictor_service
 
-    async def execute(self, norad_id: int, lat: float, lon: float) -> List[Dict]:
-        # Busca o último TLE no banco
+    async def execute(self, norad_id: int, lat: float, lon: float) -> Dict:
         satellite = await self.satellite_dataprovider.find_latest_by_norad_id(norad_id)
-        
-        if not satellite:
-            return []
 
-        # Calcula as passagens
-        passes = self.predictor_service.predict_passes(
+        if not satellite:
+            # Retorno seguindo o padrão de erro da sua aplicação
+            return {"type": "ERROR", "message": f"Satélite {norad_id} não encontrado."}
+
+        # O PredictorService agora garante tipos nativos do Python
+        return self.predictor_service.predict_passes(
             satellite.sat_tle_line_one,
             satellite.sat_tle_line_two,
             lat,
             lon
         )
-
-        return passes
